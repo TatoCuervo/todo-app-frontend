7c91f9fc24b02069111ac0c5ef283f8baeef09b0
Cristian Lopez <lopezcristianj@gmail.com>
2020-11-11 12:20:44 +1300

    Add error page

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+function Error() {
+  return (
+  <Grid container direction='column' alignItems='center'>
+    <h1>Sorry! The page does not exists</h1>
+  </Grid>
+  );
+}
+
+export default Error;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Login from '../components/Login/Login';
 import Welcome from '../components/Welcome/Welcome';
+import Error from '../components/Error/Error';
 
 const routes = (
   <Switch>
     <Redirect exact from={'/'} to={'/login'} />
     <Route exact path={'/login'} component={Login} />
     <Route exact path={'/welcome'} component={Welcome} />
+    <Route exact path='' component={Error} />
   </Switch>
 );
 export default routes;
