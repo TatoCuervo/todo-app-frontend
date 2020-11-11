c277b09ddd95ba5a2cf705c46397c852a74ccc30
Cristian Lopez <lopezcristianj@gmail.com>
2020-11-12 12:33:15 +1300

    Add axios & redux-logger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers/rootReducer';
 import './index.css';
@@ -11,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
   <Provider store={store}>
