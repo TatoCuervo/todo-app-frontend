f4366c14e51ebb2b76bf22aec39581e96ca2aaf7
Cristian Lopez <lopezcristianj@gmail.com>
2020-12-07 17:28:19 +1300

    Add material table to display TODO list

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import MaterialTable from "material-table";
+import tableIcons from '../../commons/tableIcons/tableIcons';
 import { Grid, Button } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import api from '../../api/api';
 import axios from 'axios';
 
@@ -34,6 +37,16 @@ function Welcome(props) {
   return (
     <Grid container direction='column' alignItems='center' spacing={2}>
       <h3>Welcome {props.username}</h3>
+      {loading && <CircularProgress />}
+      <MaterialTable icons={tableIcons}
+      columns={[
+        { title: "Description", field: "description" },
+        { title: "Done", field: "done" },
+        { title: "Due Date", field: "targetDate" },
+      ]}
+      data={todos}
+      title="TODO List"
+      />
       <Button variant='contained' color='primary' onClick={handleLogout}>
         Logout
       </Button>
