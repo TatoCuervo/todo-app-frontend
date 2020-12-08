57095652f9f5859e56c753d39f17a5ba812ac43d
Cristian Lopez <lopezcristianj@gmail.com>
2020-12-08 15:22:57 +1300

    Add material-table and mack handlers to add, update, delete row

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
-import {connect} from 'react-redux';
-import MaterialTable from "material-table";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import MaterialTable from 'material-table';
 import tableIcons from '../../commons/tableIcons/tableIcons';
-import { Grid, Button } from '@material-ui/core';
+import { Grid, Button, Snackbar } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import api from '../../api/api';
 import axios from 'axios';
@@ -10,6 +11,7 @@ import axios from 'axios';
 function Welcome(props) {
   let history = props.history;
   const [loading, setLoading] = useState(true);
+  const [iserror, setIserror] = useState(false);
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
@@ -21,6 +23,10 @@ function Welcome(props) {
           console.log(response.data);
           setTodos(response.data);
           setLoading(false);
+        })
+        .catch((error) => {
+          setLoading(false);
+          setIserror(true);
         });
     }
     if (loading) {
@@ -33,28 +39,72 @@ function Welcome(props) {
     localStorage.clear();
     history.push('/login');
   };
-  
+
   return (
-    <Grid container direction='column' alignItems='center' spacing={2}>
-      <h3>Welcome {props.username}</h3>
-      {loading && <CircularProgress />}
-      <MaterialTable icons={tableIcons}
-      columns={[
-        { title: "Description", field: "description" },
-        { title: "Done", field: "done" },
-        { title: "Due Date", field: "targetDate" },
-      ]}
-      data={todos}
-      title="TODO List"
-      />
-      <Button variant='contained' color='primary' onClick={handleLogout}>
-        Logout
-      </Button>
-    </Grid>
+    <>
+      <Grid container direction='column' alignItems='center' spacing={2}>
+        <h3>Welcome {props.username}</h3>
+        {loading && <CircularProgress />}
+        <MaterialTable
+          icons={tableIcons}
+          columns={[
+            { title: 'id', field: 'id', hidden: true },
+            { title: 'Description', field: 'description' },
+            { title: 'Done', field: 'done' },
+            { title: 'Due Date', field: 'targetDate' },
+          ]}
+          data={todos}
+          editable={{
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve) => {
+                handleRowUpdate(newData, oldData, resolve);
+              }),
+            onRowAdd: (newData) =>
+              new Promise((resolve) => {
+                handleRowAdd(newData, resolve);
+              }),
+            onRowDelete: (oldData) =>
+              new Promise((resolve) => {
+                handleRowDelete(oldData, resolve);
+              }),
+          }}
+          options={{
+            headerStyle: {
+              backgroundColor: '#01579b',
+              color: '#FFF',
+            },
+          }}
+          title='TODO List'
+        />
+        <Button variant='contained' color='primary' onClick={handleLogout}>
+          Logout
+        </Button>
+      </Grid>
+      <Snackbar open={iserror} autoHideDuration={2000}>
+        <MuiAlert elevation={6} variant='filled' severity='error'>
+          Unable to fetch Todo list
+        </MuiAlert>
+      </Snackbar>
+    </>
   );
 }
 
-const mapStateToProps = state => {
+const handleRowUpdate = (newData, oldData, resolve) => {
+  console.log('handleRowUpdate');
+  resolve();
+};
+
+const handleRowAdd = (newData, oldData, resolve) => {
+  console.log('handleRowAdd');
+  resolve();
+};
+
+const handleRowDelete = (newData, oldData, resolve) => {
+  console.log('handleRowDelete');
+  resolve();
+};
+
+const mapStateToProps = (state) => {
   return { username: state.auth.user };
 };
 
